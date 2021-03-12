8f38a5656b0a768a6eb1fe9c21957cecab7faa7b
Brian Steele <brian@briansteele.ca>
2021-03-12 11:00:10 -0500

    Add PRISMIC_ENDPOINT env variable

--- prismic-config.js
+++ prismic-config.js
@@ -1,6 +1,7 @@
 import Prismic from 'prismic-javascript';
+require('dotenv').config();
 
-export const apiEndpoint = "https://my-new-prismic-repo.prismic.io/api/v2";
+export const apiEndpoint = process.env.PRISMIC_ENDPOINT;
 
 export const Client = Prismic.client(apiEndpoint);
 
